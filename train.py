4090663ec7aa112be29129d79cb203896bbde57b
sakoda <sakoda@keio.jp>
2021-07-31 07:15:42 +0900

    経過時間の表示を追加

--- train.py
+++ train.py
@@ -1,21 +1,24 @@
 from my_lstm import MyLSTM
 import torch
 from lstm import LSTM
+import time
 
 MAX_STEP = 50000
 BATCH_SIZE = 256
-N = 10
-M = 11
+N = 11
+M = 12
 
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 print(f"device = {device}")
 
 hidden_size = 128
-# model = LSTM(M, hidden_size)
-model = MyLSTM(M, hidden_size)
+model = LSTM(M, hidden_size)
+# model = MyLSTM(M, hidden_size)
 model.to(device)
 crit = torch.nn.CrossEntropyLoss()
 optim = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+start = time.time()
 
 for i in range(1, MAX_STEP + 1):
     x = torch.randint(1, M, (BATCH_SIZE, N)).to(device)
@@ -27,7 +30,8 @@ for i in range(1, MAX_STEP + 1):
     teacher = x.view(-1)
 
     loss = crit(logit, teacher)
-    print(f"{i:4d}\t{loss.item():.4f}", end="\r")
+    elapsed = time.time() - start
+    print(f"{elapsed:5.1f}\t{i:4d}\t{loss.item():.4f}", end="\r")
 
     if i % (MAX_STEP // 25) == 0:
         print()
